e0316a1cbbfe575db852b069ee8cb613397253ad
Simun Strukan <simun.strukan@gmail.com>
2013-12-04 13:33:10 +0100

    Styling read me, and index page

--- views/index.php
+++ views/index.php
@@ -3,7 +3,7 @@
 		<div class="content">
 			<h2>Welcome!</h2>
 			<p class="text">This is a small codeiginter like "framework" following MVC programing paradigm. It's primary purpose is to help setting up small staging examples that we use in creating prototypes of web applications and/or dynamic websites.</p>
-			<p class="warning"><b>Warning:</b> If used in production environment, keep in mind there is work to be done! There are no security checks when inputing data, except standard PDO injection protection and no error handling for various states. They will be or will not be integrated in the future, for some contribuione visit github.</p> 			
+			<p class="warning"><b>Warning:</b> If used in production environment, keep in mind there is work to be done! There are no security checks when inputing data, except standard PDO injection protection and no error handling for various states. They will be or will not be integrated in the future, for some contribuione visit <a href="https://github.com/theHyperactive/HyperMVC">github</a>.</p> 			
 			<p class="text">There are some examples for basic controller, model, view setup. Browse the file structure! Everything should be configured in the config folder</p>
 		</div>	
 	</div>
