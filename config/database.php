bd3f1662ae164a225f9e35742c2e4aed2c4219a2
Simun Strukan <simun.strukan@gmail.com>
2013-12-04 13:21:58 +0100

    push setup

--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-/* Setup database parameters */
-
-$database['host'] 				 = '';
-$database['database_name'] = '';
-$database['username'] 		 = '';
-$database['password'] 		 = '';
-$database['port'] 				 = 3306;
